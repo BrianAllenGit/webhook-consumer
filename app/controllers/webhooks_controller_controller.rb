1923a67624d117c8f99e350184fc9d8d07241661
Brian <brianallen5193@gmail.com>
2018-01-30 14:51:58 -0500

    Remove unused header

--- app/controllers/webhooks_controller_controller.rb
+++ app/controllers/webhooks_controller_controller.rb
@@ -1,6 +1,4 @@
 class WebhooksControllerController < ApplicationController
-  skip_before_filter :verify_authenticity_token
-
   def receive
     puts "<<<<<<<<<<< PRODUCT CREATED WEBHOOK INITIATED >>>>>>>>>>>>>>>"
     puts params
