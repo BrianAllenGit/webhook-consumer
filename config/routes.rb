96d1296c67e1d922c783d9f0383f1b49193e1ed0
Brian <brianallen5193@gmail.com>
2018-01-30 17:49:08 -0500

    Display customers on home page

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-
   post '/webhooks/' => 'inbound_webhooks#receive'
-  root 'inbound_webhooks#index'
 
+  resources :customers
+  root 'inbound_webhooks#index'
 end
