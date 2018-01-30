a50fa248047fcff05fa649723a3616e30f21045c
Brian <brianallen5193@gmail.com>
2018-01-30 17:23:18 -0500

    Add a customer model

--- app/controllers/inbound_webhooks_controller.rb
+++ app/controllers/inbound_webhooks_controller.rb
@@ -4,11 +4,16 @@ class InboundWebhooksController < ApplicationController
   def receive
     puts "<<<<<<<<<<< PRODUCT CREATED WEBHOOK INITIATED >>>>>>>>>>>>>>>"
     puts params
-    puts params[:data]
-
-    Rails.logger.info params
-    Rails.logger.info params[:data]
-
+    customer_params = params[:data].dig('customer')
+    Customer.find_or_create_by!{
+      first_name: customer_params.dig('first_name'),
+      last_name: customer_params.dig('last_name'),
+    ).tap do |customer|
+      customer.email = customer_params.dig('email')
+      customer.date_of_birth = customer_params.dig('date_of_birth')
+      customer.points_balance = customer_params.dig('points_balance')
+      customer.save!
+    end
     status = rand(0..50) == 50 ?  rand(200..500) : 200
     render status: status, json: @controller.to_json
   end
