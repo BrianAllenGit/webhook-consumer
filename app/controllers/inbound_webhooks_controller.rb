c6f0311e421200233240de4f504b7a6db68d7402
Brian <brianallen5193@gmail.com>
2018-01-30 16:46:10 -0500

    Pushing

--- app/controllers/inbound_webhooks_controller.rb
+++ app/controllers/inbound_webhooks_controller.rb
@@ -4,7 +4,11 @@ class InboundWebhooksController < ApplicationController
   def receive
     puts "<<<<<<<<<<< PRODUCT CREATED WEBHOOK INITIATED >>>>>>>>>>>>>>>"
     puts params
+    puts params[:data]
+
     Rails.logger.info params
+    Rails.logger.info params[:data]
+
     status = rand(0..50) == 50 ?  rand(200..500) : 200
     render status: status, json: @controller.to_json
   end
